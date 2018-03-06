1ace04326710d429becd3ea3edc31b880447b0eb
erezd <erezdamari7@gmail.com>
2018-03-06 16:48:11 +0200

    simplify connection to service

--- SearchEngine/ConnectToEbay.cs
+++ SearchEngine/ConnectToEbay.cs
@@ -9,17 +9,16 @@ namespace SearchEngine
 {
     public class ConnectToEbay : FindingService
     {
+        private readonly string m_APPID = "ErezDama-Shos-PRD-45d80d3bd-0db6688b";
+
         protected override System.Net.WebRequest GetWebRequest(Uri uri)
         {
             try
             {
                 HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(uri);
-                request.Headers.Add("X-EBAY-SOA-SECURITY-APPNAME", "ErezDama-Shos-PRD-45d80d3bd-0db6688b");
+                request.Headers.Add("X-EBAY-SOA-SECURITY-APPNAME", m_APPID);
                 request.Headers.Add("X-EBAY-SOA-OPERATION-NAME", "findItemsByKeywords");
-                request.Headers.Add("X-EBAY-SOA-SERVICE-NAME", "FindingService");
-                request.Headers.Add("X-EBAY-SOA-MESSAGE-PROTOCOL", "SOAP11");
-                request.Headers.Add("X-EBAY-SOA-SERVICE-VERSION", "1.0.0");
-                request.Headers.Add("X-EBAY-SOA-GLOBAL-ID", "EBAY-US");
+                request.Headers.Add("X-EBAY-SOA-GLOBAL-ID", "EBAY-GB");
                 return request;
             }
             catch (Exception ex)
